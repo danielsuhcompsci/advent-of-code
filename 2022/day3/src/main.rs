a975342f7a01c25318ca86fb0f109e82a2e0f7c1
danielsuhcompsci <danielsuh112@yahoo.com>
2022-12-27 21:42:51 -0600

    part 2

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -44,12 +44,12 @@ fn print_rucksack(items: &Vec<char>, mid: &usize) {
     println!();
 }
 
-fn part_1(reader: BufReader<File>) {
+fn part_1(lines: &Vec<String>) {
     let mut sum: u32 = 0;
 
     // For each rucksack
-    for line in reader.lines() {
-        let mut item_types: Vec<char> = line.unwrap().chars().collect();
+    for line in lines {
+        let mut item_types: Vec<char> = line.chars().collect();
         let mid = item_types.len() / 2;
 
         // Sort second compartment for binary search
@@ -80,11 +80,42 @@ fn part_1(reader: BufReader<File>) {
     println!("Part 1: {}", sum);
 }
 
+fn part_2(lines: &Vec<String>) {
+    let mut sum: u32 = 0;
+
+    for i in (0..lines.len()).step_by(3) {
+        let first: Vec<char> = lines[i].chars().collect();
+        let mut second: Vec<char> = lines[i + 1].chars().collect();
+        let mut third: Vec<char> = lines[i + 2].chars().collect();
+
+        second.sort_by(|a, b| priority(a).cmp(&priority(b)));
+        third.sort_by(|a, b| priority(a).cmp(&priority(b)));
+
+        for item_type in first.iter() {
+            if b_search(
+                item_type,
+                &second,
+                0,
+                (second.len() - 1).try_into().unwrap(),
+            ) {
+                if b_search(item_type, &third, 0, (third.len() - 1).try_into().unwrap()) {
+                    sum += priority(item_type);
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("Part 2: {}", sum);
+}
+
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
 
     let file = File::open("input.txt").expect("input not found");
     let reader = BufReader::new(file);
+    let lines: Vec<String> = reader.lines().into_iter().map(|r| r.unwrap()).collect();
 
-    part_1(reader)
+    part_1(&lines);
+    part_2(&lines);
 }
