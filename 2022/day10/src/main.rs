594b02e3b48c815fce31889bd9f84d0479a73285
danielsuhcompsci <danielsuh112@yahoo.com>
2023-01-09 23:36:46 -0600

    day 10 untested

--- 2022/day10/src/main.rs
+++ 2022/day10/src/main.rs
@@ -30,6 +30,7 @@ fn part_1(lines: &Vec<String>) {
     let mut queue: VecDeque<Process> = VecDeque::new();
 
     for (i, line) in lines.iter().enumerate() {
+        // Parse input
         let split: Vec<&str> = line.split(' ').collect();
         match split[0] {
             "addx" => {
@@ -39,11 +40,7 @@ fn part_1(lines: &Vec<String>) {
             _ => {}
         }
 
-        // Calculate and add signal strength
-        if i + 1 - 20 % 40 == 0 && i < 220 {
-            sum += register * (i as i32 + 1);
-        }
-
+        // Check if the first process in queue is done, if so, pop off queue and add to register
         match queue.front() {
             Some(process) => {
                 if process.is_done() {
@@ -55,17 +52,27 @@ fn part_1(lines: &Vec<String>) {
             None => {}
         }
 
+        // Calculate and add signal strength if 20th, 60th, 100th ... 220th cycle
+        if i + 1 - 20 % 40 == 0 && i < 220 {
+            sum += register * (i as i32 + 1);
+        }
+
+        // At the end of cycle decrement counter for each process in queue
         for process in queue.iter_mut() {
             process.decrement();
         }
     }
+
+    println!("Part 1: {}", sum);
 }
 
 fn part_2() {}
 
 fn main() {
-    let file = File::create("./input.txt").unwrap();
+    let file = File::create("input.txt").unwrap();
     let reader = BufReader::new(file);
 
     let lines: Vec<String> = reader.lines().map(|r| r.unwrap()).collect();
+
+    part_1(&lines);
 }
