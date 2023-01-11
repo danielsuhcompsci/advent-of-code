1f3e9b06d2a41ce3287e3fdcf11f8e7427589290
danielsuhcompsci <danielsuh112@yahoo.com>
2023-01-10 20:46:56 -0600

    day 10 part 1

--- 2022/day10/src/main.rs
+++ 2022/day10/src/main.rs
@@ -4,22 +4,29 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-struct Process {
-    pub value: i32,
-    pub counter: usize,
-}
+// struct Process {
+//     pub value: i32,
+//     pub counter: usize,
+// }
 
-impl Process {
-    pub fn new(value: i32) -> Self {
-        Process { value, counter: 2 }
-    }
+// impl Process {
+//     pub fn new(value: i32) -> Self {
+//         Process { value, counter: 2 }
+//     }
 
-    pub fn decrement(&mut self) {
-        self.counter -= 1;
-    }
+//     pub fn decrement(&mut self) {
+//         self.counter -= 1;
+//     }
 
-    pub fn is_done(&self) -> bool {
-        self.counter == 0
+//     pub fn is_done(&self) -> bool {
+//         self.counter == 0
+//     }
+// }
+
+fn calc_signal_strength_if(sum: &mut i32, register: &i32, cycle: &usize) {
+    // Calculate and add signal strength if 20th, 60th, 100th ... 220th cycle
+    if (*cycle as i32 - 20) % 40 == 0 && *cycle <= 220 {
+        *sum += register * (*cycle as i32);
     }
 }
 
@@ -27,39 +34,28 @@ fn part_1(lines: &Vec<String>) {
     let mut sum = 0;
     let mut register: i32 = 1;
 
-    let mut queue: VecDeque<Process> = VecDeque::new();
+    // let mut queue: VecDeque<Process> = VecDeque::new();
 
-    for (i, line) in lines.iter().enumerate() {
+    let mut cycle: usize = 1;
+
+    for line in lines.iter() {
+        calc_signal_strength_if(&mut sum, &register, &cycle);
+
         // Parse input
         let split: Vec<&str> = line.split(' ').collect();
         match split[0] {
             "addx" => {
                 let value: i32 = split[1].parse().unwrap();
-                queue.push_back(Process::new(value));
+
+                cycle += 1;
+                calc_signal_strength_if(&mut sum, &register, &cycle);
+
+                cycle += 1;
+                register += value;
             }
-            _ => {}
-        }
-
-        // Check if the first process in queue is done, if so, pop off queue and add to register
-        match queue.front() {
-            Some(process) => {
-                if process.is_done() {
-                    register += process.value;
-                }
-
-                queue.pop_front();
+            _ => {
+                cycle += 1;
             }
-            None => {}
-        }
-
-        // Calculate and add signal strength if 20th, 60th, 100th ... 220th cycle
-        if i + 1 - 20 % 40 == 0 && i < 220 {
-            sum += register * (i as i32 + 1);
-        }
-
-        // At the end of cycle decrement counter for each process in queue
-        for process in queue.iter_mut() {
-            process.decrement();
         }
     }
 
@@ -69,7 +65,7 @@ fn part_1(lines: &Vec<String>) {
 fn part_2() {}
 
 fn main() {
-    let file = File::create("input.txt").unwrap();
+    let file = File::open("./input.txt").expect("Unable to open file");
     let reader = BufReader::new(file);
 
     let lines: Vec<String> = reader.lines().map(|r| r.unwrap()).collect();
