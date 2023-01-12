38d340f11f3b3b65b2223489e4fdc2e9e60a84ac
danielsuhcompsci <danielsuh112@yahoo.com>
2023-01-11 22:36:27 -0600

    day 10

--- 2022/day10/src/main.rs
+++ 2022/day10/src/main.rs
@@ -62,7 +62,54 @@ fn part_1(lines: &Vec<String>) {
     println!("Part 1: {}", sum);
 }
 
-fn part_2() {}
+fn print_pixel(sprite_position: i32, cycle: usize) {
+    let cycle_x = match cycle % 40 {
+        0 => cycle - 1,
+        _ => cycle % 40 - 1,
+    };
+
+    if sprite_position - 1 <= cycle_x as i32 && cycle_x as i32 <= sprite_position + 1 {
+        print!("#");
+    } else {
+        print!(".");
+    }
+
+    if cycle % 40 == 0 {
+        println!();
+    }
+}
+
+fn part_2(lines: &Vec<String>) {
+    println!("Part 2:");
+
+    let mut register: i32 = 1;
+
+    // let mut queue: VecDeque<Process> = VecDeque::new();
+
+    let mut cycle: usize = 1;
+
+    for line in lines.iter() {
+        print_pixel(register, cycle);
+
+        // Parse input
+        let split: Vec<&str> = line.split(' ').collect();
+        match split[0] {
+            "addx" => {
+                let value: i32 = split[1].parse().unwrap();
+
+                cycle += 1;
+
+                print_pixel(register, cycle);
+
+                cycle += 1;
+                register += value;
+            }
+            _ => {
+                cycle += 1;
+            }
+        }
+    }
+}
 
 fn main() {
     let file = File::open("./input.txt").expect("Unable to open file");
@@ -71,4 +118,5 @@ fn main() {
     let lines: Vec<String> = reader.lines().map(|r| r.unwrap()).collect();
 
     part_1(&lines);
+    part_2(&lines);
 }
